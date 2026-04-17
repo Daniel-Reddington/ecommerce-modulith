8957a49d5acae60e0f8e53f0d33c6ab36cf81190
Daniel-Reddington <mialinirinadaniela@gmail.com>
2026-04-17 21:51:53 +0300

    testing create, get and get all products

---
+++ src/test/java/com/learning/ecommerce/product/application/usecase/GetAllProductUseCaseTest.java
@@ -0,0 +1,47 @@
+package com.learning.ecommerce.product.application.usecase;
+
+import com.learning.ecommerce.product.application.port.ProductRepository;
+import com.learning.ecommerce.product.domain.model.Product;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GetAllProductUseCaseTest {
+
+    @Test
+    public void should_return_all_products(){
+        ProductRepository repository = mock(ProductRepository.class);
+
+        GetAllProductsUseCase useCase = new GetAllProductsUseCase(repository);
+
+        List<Product> products = List.of(
+                Product.create("Laptop", 1000D),
+                Product.create("Samsung Galaxy", 2000D)
+        );
+
+        when(repository.findAll()).thenReturn(products);
+
+        List<Product> results = useCase.execute();
+
+        assertEquals(2, results.size());
+        assertEquals("Laptop", results.getFirst().name());
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    public void should_return_empty_list_when_no_products(){
+        ProductRepository repository = mock(ProductRepository.class);
+        GetAllProductsUseCase useCase = new GetAllProductsUseCase(repository);
+
+        when(repository.findAll()).thenReturn(List.of());
+
+        List<Product> results = useCase.execute();
+
+        assertTrue(results.isEmpty());
+        verify(repository).findAll();
+    }
+}
---
+++ src/test/java/com/learning/ecommerce/product/application/usecase/GetProductUseCaseTest.java
@@ -0,0 +1,54 @@
+package com.learning.ecommerce.product.application.usecase;
+
+import com.learning.ecommerce.product.application.port.ProductRepository;
+import com.learning.ecommerce.product.domain.exception.ProductNotFoundException;
+import com.learning.ecommerce.product.domain.model.Product;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GetProductUseCaseTest {
+
+    @Test
+    public void should_return_product_when_found(){
+
+        ProductRepository repository = mock(ProductRepository.class);
+
+        UUID id = UUID.randomUUID();
+
+        Product product = Product.create("Laptop", 1000D);
+
+        when(repository.findById(id)).thenReturn(Optional.of(product));
+
+        GetProductUseCase getProductUseCase = new GetProductUseCase(repository);
+
+        Product result =  getProductUseCase.execute(id);
+
+        assertNotNull(result);
+
+        assertEquals(product.name(), result.name());
+
+        verify(repository).findById(id);
+
+    }
+
+
+    @Test
+    public void should_throw_exception_when_product_not_found(){
+        ProductRepository repository = mock(ProductRepository.class);
+
+        GetProductUseCase useCase = new GetProductUseCase(repository);
+
+        UUID id = UUID.randomUUID();
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(ProductNotFoundException.class, () -> useCase.execute(id));
+
+        verify(repository).findById(id);
+
+    }
+}
